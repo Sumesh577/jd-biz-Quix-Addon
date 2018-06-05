07acf051afb03fe4b939706d6d79106d97ebc43e
joomgmanish <38451135+joomgmanish@users.noreply.github.com>
2018-06-05 12:12:55 +0530

    changes in social profile

--- jd-team/view.php
+++ jd-team/view.php
@@ -11,19 +11,10 @@
 			<div class="team-member-wrapper text-center">
 				 <div class="team-member-image <?php echo $field['image_style']?> mb-2">
 					  <img src="<?php echo $field['image']?>" alt="<?php echo $field['name']?>">
-					  <div class="team-social-profile d-flex">
-						  <?php if($field['social-f'])  { ?>
-						   <a href="<?php  echo $field['social-f']; ?>"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-						  <?php } ?>
-						  <?php if($field['social-t'])  { ?>
-						   <a href="<?php  echo $field['social-t']; ?>"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-						   <?php } ?>
-						   <?php if($field['social-i'])  { ?>
-						   <a href="<?php  echo $field['social-i']; ?>"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-						   <?php } ?>
-						   <?php if($field['social-g'])  { ?>
-						   <a href="<?php  echo $field['social-g']; ?>"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-						   <?php } ?>
+					  <div class="team-social-profile d-flex social-links">
+						 <?php foreach($field['social'] as $link):?>
+      						<a class="qx-icon" href="<?php echo $link?>" target="_blank" rel="noopener noreferrer"></a>
+    					 <?php endforeach;?>
 					  </div><!-- End team social profile -->
 					  <div class="team-overly"></div>
 				 </div><!-- End Team member image -->
